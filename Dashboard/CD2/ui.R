75ef322f0b457c19d3158aeece3a7f2955260db6
Step_Advancement <Step_Advancement@mhc.ab.ca>
2019-08-15 16:01:53 -0600

    more changes to FAQ

--- Dashboard/CD2/ui.R
+++ Dashboard/CD2/ui.R
@@ -12,6 +12,52 @@ shinyUI(
       useShinyjs(),
       theme = shinytheme("paper"),
       
+      tags$style("#mainChart {
+                 margin-bottom: 50px;
+                 "),
+      
+      tags$style("#quick-tips {
+                  margin-top: 20px;
+                 "),
+      
+      tags$style("#title2 {
+                  float: left;
+                  margin-bottom: 10px;
+                  text-shadow: 1px 1px 5px #D8D8D8;
+                 }"),
+
+      tags$style("#context-banner2 {
+                  padding: 5px 20px;
+                  background-color: white;
+                  float:left;
+                  width: 100%;
+                  box-shadow: 0px 5px 6px -1px #D8D8D8;
+                  border-top: 1px;
+                  border-radius: 3px;
+                  margin: 10px 0px;
+                 }"),
+
+      
+      tags$style("#welcome-text2 {
+                  border-top: 1px solid #D8D8D8;
+                  padding-top: 20px;
+                  float:left;
+                  width: 70%;
+                  margin-top: 10px;
+                 }"),
+      
+      tags$style("#car-image2 {
+                  float: right;
+                  width: 25%;
+                  margin: 10px;
+                  border-radius: 5px;
+                  box-shadow: 1px 1px 6px 2px #D8D8D8;
+                 }"),
+      
+      tags$style(".main-container {
+                 box-shadow: 1px 1px 6px 2px #D8D8D8 inset;
+                }"),
+      
       tags$style(".selectize-input.focus {
              border-color: #2196f3;
              border-width: 1.5px;
@@ -46,24 +92,48 @@ shinyUI(
               font-weight: bold;, 
               font-size: large}"),
       
-      titlePanel(HTML("<font color = '#2196f3'>Volkswagen</font>
-                  <b>E-Golf</b> Data Explorer"), 
-                 windowTitle = "EVDB"),
+      #tags$h2(id="title", 
+       #       tags$span("Volkswagen", style = "color: #2196f3"),
+        #      tags$span("E-Golf", style = "font-weight: bold;"),
+         #     "Data Explorer"
+      #),
       
-      #tags$img()
+      #tags$div(id="separator-1"),
+      
+      #tags$img(id="car-image", src = "EV_439.jpg", alt="Volkswagen_logo.png"),
       
       #withTags({
-        #div(class="header",
-          #p("Welcome to the Medicine Hat College Electric Vehicle Dashboard! Last November,
-           # MHC partnered with Southland Volkswagen in order to learn about electric vehicles.")
-          #img()
+      #  div(id="context-banner",
+       #   p(id="welcome-text", "Welcome to the Medicine Hat College Electric Vehicle Dashboard! Last November,
+        #    MHC partnered with Southland Volkswagen in order to learn about electric vehicles. 
+         #   This tool was created to answer some common questions about the E-golf and electric vehicles,
+          #  and to allow anyone to explore the data that we have collected over the course of this project.")
         #)
-      #})
+      #}),
+      
+      tags$div(id="separator-test"),
+      
+      withTags({
+        div(id="context-banner2",
+            h2(id="title2", 
+              span("Volkswagen", style = "color: #2196f3"),
+              span("E-Golf", style = "font-weight: bold;"),
+              "Data Explorer"
+            ),
+            img(id="car-image2", src = "EV_439.jpg", alt="Volkswagen_logo.png"),
+            p(id="welcome-text2", "Welcome to the Medicine Hat College Electric Vehicle Dashboard! Last November,
+            MHC partnered with Southland Volkswagen in order to learn about electric vehicles. 
+            This tool was created to answer some common questions about the E-golf and electric vehicles,
+            and to allow anyone to explore the data that we have collected over the course of this project.")
+        )
+      }),
+      
+      tags$div(id="separator-2"),
       
       tabsetPanel(
         tabPanel("Common Questions",
                  icon = icon("book-reader"),
-                 tags$iframe(src = 'FAQ2.html', 
+                 tags$iframe(src = 'FAQ11.html', 
                              width = '100%', height = '800px', 
                              frameborder = 0, scrolling = 'auto')
         ),
@@ -115,13 +185,22 @@ shinyUI(
                                 
                                 checkboxInput("facet_plot", "Split Groups", value = F),
                                 
-                                helpText("Note: A blue line of best fit will appear on
-                                         the chart when the relationship between the two 
-                                         chosen variables is strong enough."),
+                                bookmarkButton(label = "Save", icon = icon("bookmark")),
                                 
-                                bookmarkButton(label = "Save", icon = icon("bookmark"))
+                                tags$div(id="quick-tips",
+                                         tags$h6("Quick Tips:"),
+                                         tags$p("1. A strong relationship will cause a blue line
+                                                of best fit to display"),
+                                         tags$p("2. Hide/show groups by clicking the group names in the legend"),
+                                         tags$p("3. Use the mouse to select an area to be zoomed,
+                                                double click the chart to zoom back out"),
+                                         tags$p("Check the help tab for more information about using the tool")
+                                )
+                                
+                                
                                 
                               ),
+                              
                               
                               mainPanel(width = 9,
                                 plotlyOutput("mainChart",
@@ -129,6 +208,8 @@ shinyUI(
                                               width = "100%"
                                 )
                               )
+                              
+        
                             )
         ),
         
